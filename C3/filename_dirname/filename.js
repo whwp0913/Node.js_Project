aec5a4942ef166b6a428b47d03326b461d375c50
whwp0913 <whwp0913@gmail.com>
2018-09-04 16:13:22 +0900

    node_object change

--- C3/filename_dirname/filename.js
+++
@@ -1,7 +0,0 @@
-/*
-filename은 루트부터 ~ 파일명까지
-dirname은 루트부터 ~ 폴더명까지
-*/
-console.log(__filename);
-
-console.log(__dirname);
--- C3/global/globalB.js
+++
@@ -1,6 +0,0 @@
-const A = require('./globalA');
-
-global.message = `hello`;
-
-//A는 global.message 반환
-console.log(A());
