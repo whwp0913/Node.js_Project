59dbaeb6554e930de65ff3ff41b31b429e01bbfa
whwp0913 <whwp0913@gmail.com>
2018-10-13 20:49:55 +0900

    casperjs...project

--- crwaling/main.js
+++ crwaling/main.js
@@ -7,14 +7,23 @@
 5. 해당 target의 폴더를 만들고(없는 경우) -> 제목으로 폴더 생성 -> 폴더 내에 최산화 내용 저장
 6. 반복작업
 */
-
 var casper = require('casper').create();
 
+//설정 정보
 casper.options.verbose = false;
 casper.options.logLevel = "debug";
+casper.options.viewportSize = {
+    width: 300,
+    height: 200
+};
+casper.options.pageSettings = {
+    loadImages: true,
+    loadPlugins: false
+};
 
+//요일에 해당하는 타겟들 주소
 function getUrlList() {
-    var target = document.querySelectorAll('.col_selected ul a');
+    var target = document.querySelectorAll('.col_selected ul .title');
     var list = [];
     for(var i=0; i < target.length; i++) {
         list.push(target[i].href);
@@ -22,9 +31,15 @@ function getUrlList() {
     return list;
 }
 
+//업로드 됐는지 확인여부
 function checkUpload() {
-    if()
-    var target = document.querySelectorAll('.title');
+    var checkpoint = document.querySelectorAll('img');
+
+    for(var i=0; i < checkpoint.length; i++) {
+        if(checkpoint[i].alt == 'UP') {
+            return true;
+        }
+    }
 }
 
 //웹툰
@@ -40,11 +55,28 @@ casper.then(function() {
 //요일웹툰 리스트
 casper.then(function() {
     var titleList = this.evaluate(getUrlList);
-        casper.open(targetList[0]).then(function() {
-            var check =
-            this.click()
-        })
 
+    casper.each(titleList, function(self, link) {
+        self.thenOpen(link, function() {
+            var check = this.evaluate(checkUpload);
+
+            if(check) {
+                this.click('.title a');
+
+                casper.then(function() {
+                    var day = this.getHTML('.category_tab .on a');
+                    var title = this.getTitle().trim();
+                    var subTitle = this.getHTML('.view h3').trim();
+                    casper.echo(title+"추가완료!");
+                    this.
+                    captureSelector('/Users/whwp0913/Downloads/Comic/'+day+'/'+title+'/'+subTitle+'.png', '.wt_viewer', { quality : 20 });
+                });
+            }
+        });
+    });
 });
 
-casper.run();
+casper.run(function() {
+    console.log('작업완료...');
+    casper.exit();
+});
